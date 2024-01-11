0a9aa51e90574df413cca97966d639a3b9e79a75
abhikesare9 <abhikesare9@gmail.com>
2024-01-11 21:05:09 +0530

    repeated code.

--- eks/output.tf
+++
@@ -1,5 +0,0 @@
-output cluster_url {
-  value       = aws_eks_cluster.ekscluster.endpoint
-  sensitive   = true
-  description = "eks cluster url"
-}
--- s3/output.tf
+++
@@ -1,5 +0,0 @@
-output s3_bucket {
-  value       = aws_s3_bucket.s3_bucket.id
-  sensitive   = false
-  description = "s3 bucket name"
-}
