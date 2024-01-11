9521d7757acbf0f89421ab40c64535a4ac4aea74
abhikesare9 <abhikesare9@gmail.com>
2024-01-11 21:03:07 +0530

    repeated code

--- ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output cicd_server_ip {
-  value       = aws_instance.cicd_server.public_ip
-  description = "public ip address of cicd server"
-}
-
-output monitoring_server_ip {
-  value       = aws_instance.monitoring_server.public_ip
-  description = "public ip address of monitoring server"
-}
